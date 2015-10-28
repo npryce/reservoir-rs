9410bf615150a8df8ad1bcd93d25c397ebf4f7b1
Nat Pryce <sw@natpryce.com>
2015-10-28 14:54:51 +0000

    converted unit tests to doctests

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,40 @@ use rand::Rng;
 /// Return a random sample of a known maximum size from an iterator of unknown length.
 ///
 /// Implements Jeffrey Vitter's Algorithm R (see https://en.wikipedia.org/wiki/Reservoir_sampling)
+///
+/// # Examples
+/// 
+/// ```
+/// # extern crate rand;
+/// # extern crate reservoir;
+/// # use rand::thread_rng;
+/// # use reservoir::sample;
+/// # fn main() {
+/// let iter = 0..10;
+/// 
+/// let samples = sample(&mut thread_rng(), 4, iter);
+/// 
+/// assert_eq!(4, samples.len());
+/// assert!(samples.iter().all(|e| *e >= 0 && *e < 10));
+/// # }
+/// ```
+/// 
+/// If the sampled iterator contains fewer items than the sample_size, all items are returned.
+///
+/// ```
+/// # extern crate rand;
+/// # extern crate reservoir;
+/// # use rand::thread_rng;
+/// # use reservoir::sample;
+/// # fn main() {
+/// let iter = 0..10;
+/// 
+/// let samples : Vec<i32> = sample(&mut thread_rng(), 20, iter);
+/// let expected_samples : Vec<i32> = (0..10).collect();
+/// 
+/// assert_eq!(expected_samples, samples);
+/// # }
+/// ```
 
 pub fn sample<I, RNG>(rng : &mut RNG, sample_size : usize, iter : I) -> Vec<I::Item>
     where I : Iterator,
@@ -29,30 +63,4 @@ pub fn sample<I, RNG>(rng : &mut RNG, sample_size : usize, iter : I) -> Vec<I::I
     }
 
     samples
-}
-
-#[cfg(test)]
-mod test {
-    use rand::thread_rng;
-    use super::sample;
-    
-    #[test]
-    fn sampling() {
-        let iter = 0..10;
-        
-        let samples = sample(&mut thread_rng(), 4, iter);
-        
-        assert_eq!(4, samples.len());
-        assert!(samples.iter().all(|e| *e >= 0 && *e < 10));
-    }
-    
-    #[test]
-    fn sample_size_larger_than_iter() {
-        let iter = 0..10;
-        
-        let samples : Vec<i32> = sample(&mut thread_rng(), 20, iter);
-        let expected_samples : Vec<i32> = (0..10).collect();
-        
-        assert_eq!(expected_samples, samples);
-    }
 }
